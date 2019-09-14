e957c257f7d88e201e4a8d0cf22f8009054bcf3d
mrahman1997 <42300710+mrahman1997@users.noreply.github.com>
2019-09-14 12:18:13 -0400

    edits

--- Data Cleaning/climate_change_data_cleaning.r
+++ Data Cleaning/climate_change_data_cleaning.r
@@ -4,8 +4,6 @@ library(skimr)
 library(janitor)
 
 data <- fread("data.csv", header = T)
-country <- fread("country.csv")
-series <- fread("series.csv")
 
 skim(data)
 data <- data %>% clean_names()
@@ -21,11 +19,3 @@ g_data$year <- g_data$year %>% str_replace("x", "")
 g_data <- g_data %>% mutate(decimals = as.numeric(decimals), scale = as.numeric(scale), value = as.numeric(value))
 skim(g_data)
 
-skim(country)
-table(country$capital_city)
-country[country == ".."] <- NA
-country[country == ""] <- NA
-
-skim(series)
-df <- inner_join(g_data, country, by = "country_code")
-skim(df)
